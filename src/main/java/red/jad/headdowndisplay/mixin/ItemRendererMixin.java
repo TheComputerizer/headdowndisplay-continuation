775bfa92c1c49763cadee847f08553da0d4b4c86
TheComputerizer <dcpixelation42@gmail.com>
2023-03-15 21:13:17 -0500

    fix item number and slot effects not moving and remove debug log

--- src/main/java/red/jad/headdowndisplay/mixin/ItemRendererMixin.java
+++ src/main/java/red/jad/headdowndisplay/mixin/ItemRendererMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import red.jad.headdowndisplay.backend.HudAnimationHandler;
 
@@ -29,5 +30,12 @@ public abstract class ItemRendererMixin {
                                PoseStack matrix, MultiBufferSource buffer, int combinedLight, int combinedOverlay,
                                BakedModel model, CallbackInfo ci){
         if(itemStack != ItemStack.EMPTY && transformType == ItemTransforms.TransformType.GUI) matrix.popPose();
+    }
+
+    @ModifyVariable( method = "renderGuiItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
+            at = @At(value = "STORE"))
+    private PoseStack translateItemDecorations(PoseStack matrix) {
+        matrix.translate(0, HudAnimationHandler.getY(), 0);
+        return matrix;
     }
 }

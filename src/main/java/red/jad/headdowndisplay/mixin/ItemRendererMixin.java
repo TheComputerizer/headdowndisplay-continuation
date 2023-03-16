554b6cb998e2cf701bd07b8c379a7565851767db
TheComputerizer <dcpixelation42@gmail.com>
2023-03-15 20:08:17 -0500

    no need for mixin debugging

--- src/main/java/red/jad/headdowndisplay/mixin/ItemRendererMixin.java
+++ src/main/java/red/jad/headdowndisplay/mixin/ItemRendererMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import red.jad.headdowndisplay.backend.HudAnimationHandler;
 
 @Mixin(ItemRenderer.class)
-@Debug(print = true, export = true)
 public abstract class ItemRendererMixin {
 
     @Inject( method = "render", at = @At(value = "HEAD") )
